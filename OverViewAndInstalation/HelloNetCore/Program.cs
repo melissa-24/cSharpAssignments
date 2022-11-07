c98cc01bb05eae23229fd8f7f7f05ecc5767b2dd
melissa-24 <melissalongenberger24@gmail.com>
2022-11-06 23:46:47 -0500

    assignment 1 done

---
+++ OverViewAndInstalation/HelloNetCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Created using dotnet new console -o HelloNetCore");
+            Console.WriteLine("I could have done mkdir HelloNetCore, cd HelloNetCore, and then dotnet new console");
+            Console.WriteLine("To run the file all I need to do is dotnet run");
+        }
+    }
+}
